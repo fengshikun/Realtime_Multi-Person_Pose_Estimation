9895405807218866574098044464819958e7c2d1
root <root@localhost.localdomain>
2018-03-17 00:00:52 +0800

    add exp

---
+++ training/example_proto/mobilenet_v2/3_stages_kernel_reduce/train_pose.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+/home/shikun/Project/pose_estimation/caffe_train/build/tools/caffe train --solver=pose_solver.prototxt --gpu=6 --weights=../mobilenet_v2.caffemodel 2>&1 | tee ./output.txt
